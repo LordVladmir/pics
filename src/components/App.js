65f20900df3b3e9f8f15fb49c97264b92a7dce4b
MasterSaad <sdmamodesaeb@student.udm.ac.mu>
2020-12-15 16:34:57 +0400

    this.state async api

---
+++ src/components/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import axios from "axios";
+import SearchBar from "./SearchBar";
+
+class App extends React.Component {
+    state ={ images: []}
+
+  async onSearchSubmit(term) {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization: "Client-ID cmlLK1zH6rK30d-mQWmlfKWiJFisEtSEze1RpqUdtMY",
+      },
+    });
+
+    //     .then((response) => {
+    //         console.log(response.data.results);
+    //     });
+
+    
+    this.setState({images: response.data.results});
+  }
+
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: "10px" }}>
+        <SearchBar onSubmit={() => this.onSearchSubmit()} />
+        Found: {this.state.images.length} images
+      </div>
+    );
+  }
+}
+
+export default App;
